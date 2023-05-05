def09a838eb459f5a387eb534012c60bc8a7d0bd
fgorodkovets <f.gorodkovets@itransition.com>
2023-05-05 14:26:46 +0200

    UR-6. Dynamic styling

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,36 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
 const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+  const [enteredValue, setEnteredValue] = React.useState('');
+  const [isValid, setIsValid] = React.useState(true);
 
   const goalInputChangeHandler = event => {
+    if (event.target.value) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
-    props.onAddGoal(enteredValue);
+    if (enteredValue.trim().length > 0) {
+      props.onAddGoal(enteredValue);
+    } else {
+      setIsValid(false);
+    }
   };
 
   return (
       <form onSubmit={formSubmitHandler}>
-        <div className="form-control">
+        <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
           <label>Course Goal</label>
-          <input type="text" onChange={goalInputChangeHandler}/>
+          <input
+              type="text"
+              onChange={goalInputChangeHandler}
+          />
         </div>
         <Button type="submit">Add Goal</Button>
       </form>
