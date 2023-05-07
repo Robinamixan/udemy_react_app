eb63be53af1f4e39dea14978cb992ac1f78596ec
fgorodkovets <f.gorodkovets@itransition.com>
2023-05-07 10:55:34 +0200

    UR-7. Debugging section - initial setup

--- src/_UR_7_Debugging/index.js
+++ src/_UR_7_Debugging/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import App from './_UR_7_Debugging/App';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
+root.render(<App />);
