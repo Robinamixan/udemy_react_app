07fd64db4d0930ea3eaa2eba2ea02aaf11f35eb1
fgorodkovets <f.gorodkovets@itransition.com>
2023-05-09 15:20:12 +0200

    UR-11. Prepared new project and moved previous project ot separate folder

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import React from 'react';
-
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
-import AuthContext from './store/auth-context';
-
 function App() {
-  const context = React.useContext(AuthContext);
-
   return (
-    <React.Fragment>
-      <MainHeader />
-      <main>
-        {!context.isLoggedIn && <Login/>}
-        {context.isLoggedIn && <Home/>}
-      </main>
-    </React.Fragment>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
