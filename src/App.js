438300bb997e8a3625b253301d3f1cbdff28e460
fgorodkovets <f.gorodkovets@itransition.com>
2023-05-11 09:23:23 +0200

    UR-20. Moved food order project to separate folder and prepared initial setup for new section

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
-import React from 'react';
-
-import Header from './components/Layout/Header/Header';
-import MealsContent from './components/Meals/MealsContent/MealsContent';
-import Cart from './components/Cart/Cart/Cart';
-import CartProvider from './store/CartProvider';
-
 function App() {
-  const [isCartVisible, setIsCartVisible] = React.useState(false);
-
-  const showCartHandler = () => {
-    setIsCartVisible(true);
-  };
-
-  const hideCartHandler = () => {
-    setIsCartVisible(false);
-  };
-
   return (
-    <CartProvider>
-      {isCartVisible && <Cart onCartHide={hideCartHandler}/>}
-      <Header onCartShow={showCartHandler}/>
-      <main>
-        <MealsContent/>
-      </main>
-    </CartProvider>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
