97a3ea766ba3b5e9d05fbbbdeda4cc8dfddcd707
fgorodkovets <f.gorodkovets@itransition.com>
2023-05-22 16:10:09 +0200

    UR-13. Class based components

--- _pure_fe_project_setups/src/components/User.js
+++ _pure_fe_project_setups/src/components/User.js
@@ -1,7 +1,25 @@
-import classes from './User.module.css';
+import React, { Component } from 'react';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
+import classes from './User.module.css';
+import PropTypes from 'prop-types';
+
+class User extends Component {
+    componentWillUnmount() {
+        console.log('unmounted!');
+    }
+
+    render() {
+        return <li className={classes.user}>{this.props.name}</li>;
+    }
+}
+
+// Same Component but with functional implementation
+// const User = (props) => {
+//     return <li className={classes.user}>{props.name}</li>;
+// };
+
+User.propTypes = {
+    name: PropTypes.string,
 };
 
 export default User;
