beb232d514506b3ad35553e651bd4f34acbb8fce
fgorodkovets <f.gorodkovets@itransition.com>
2023-05-23 12:00:47 +0200

    UR-14. Http requests

--- _pure_fe_project_setups/src/App.js
+++ _pure_fe_project_setups/src/App.js
@@ -2,33 +2,79 @@ import React from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie.js';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+    const [movies, setMovies] = React.useState([]);
+    const [isLoading, setIsLoading] = React.useState(false);
+    const [error, setError] = React.useState(null);
 
-  return (
-    <React.Fragment>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
-    </React.Fragment>
-  );
+    const fetchMoviesHandler = React.useCallback(async () => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch('https://swapi.dev/api/films');
+            if (!response.ok) {
+                setError('Something went wrong');
+                setIsLoading(false);
+                return;
+            }
+
+            const responseData = await response.json();
+
+            const moviesData = responseData.results.map((movie) => {
+                return {
+                    id: movie.episode_id,
+                    title: movie.title,
+                    openingText: movie.opening_crawl,
+                    releaseDate: movie.release_date,
+                };
+            });
+
+            setMovies(moviesData);
+        } catch (error) {
+            setError(error.message);
+            console.log(error);
+        }
+
+        setIsLoading(false);
+    });
+
+    React.useEffect(() => {
+        fetchMoviesHandler();
+    }, []);
+
+    function addMovieHandler(movie) {
+        console.log(movie);
+    }
+
+    let content = <p>Found no movies.</p>;
+
+    if (error) {
+        content = <p>{error}</p>;
+    }
+
+    if (isLoading) {
+        content = <p>Loading...</p>;
+    }
+
+    if (movies.length > 0) {
+        content = <MoviesList movies={movies} />;
+    }
+
+    return (
+        <React.Fragment>
+            <section>
+                <AddMovie onAddMovie={addMovieHandler} />
+            </section>
+            <section>
+                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+            </section>
+            <section>
+                {content}
+            </section>
+        </React.Fragment>
+    );
 }
 
 export default App;
--- _pure_fe_project_setups/src/components/MoviesList.js
+++ _pure_fe_project_setups/src/components/MoviesList.js
@@ -5,22 +5,22 @@ import Movie from './Movie';
 import classes from './MoviesList.module.css';
 
 MovieList.propTypes = {
-  movies: PropTypes.array
+    movies: PropTypes.array,
 };
 
 function MovieList(props) {
-  return (
-    <ul className={classes['movies-list']}>
-      {props.movies.map((movie) => (
-        <Movie
-          key={movie.id}
-          title={movie.title}
-          releaseDate={movie.releaseDate}
-          openingText={movie.openingText}
-        />
-      ))}
-    </ul>
-  );
+    return (
+        <ul className={classes['movies-list']}>
+            {props.movies.map((movie) => (
+                <Movie
+                    key={movie.id}
+                    title={movie.title}
+                    releaseDate={movie.releaseDate}
+                    openingText={movie.openingText}
+                />
+            ))}
+        </ul>
+    );
 }
 
 export default MovieList;
