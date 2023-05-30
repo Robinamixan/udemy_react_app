9c110efdc8858bc00701e28d2ae3276e438f0c35
fgorodkovets <f.gorodkovets@itransition.com>
2023-05-30 11:31:10 +0200

    UR-18. Moved previous project to separate folder

---
+++ _pure_fe_project_setups/_UR_16_forms_and_user_input/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import BasicForm from './components/BasicForm.js';
+
+function App() {
+    return (
+        <div className="app">
+            <BasicForm />
+        </div>
+    );
+}
+
+export default App;
